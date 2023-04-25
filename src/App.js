ea87e070c7e23f46e38294da8da9c11a64becd3c
Marwahaifawi <maroosh.10102.mm@gmail.com>
2023-04-25 04:23:03 +0300

    show movies cards from API

--- src/App.js
+++ src/App.js
@@ -5,54 +5,42 @@ import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 //67ca1ad1 API Key
 
-const API_URL = "http://www.omdbapi.com?apikey=67ca1ad1"; 
+const API_URL = "http://www.omdbapi.com?apikey=67ca1ad1";
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchItem , setSearchItem] = useState('');
-  const [items, setItems] = useState([]);
+  const [searchItem, setSearchItem] = useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
-  useEffect( () =>  {
-    searchMovies('Spiderman')
+  useEffect(() => {
+    searchMovies("Spiderman");
   }, []);
 
-//  if(items.length == 0)
-//   return <div>nothing</div>
- 
   return (
     <div className="app">
       <h1>Movie Land</h1>
       <div className="search">
         <input
           placeholder="Search movies"
-          value= {searchItem}
-          onChange={(e) => setSearchItem(e.target.value)
-        }
+          value={searchItem}
+          onChange={(e) => setSearchItem(e.target.value)}
         />
-        
-        <img 
-        src={SearchIcon} 
-        alt="search" 
-        onClick={() => searchMovies} 
-        />
+
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies} />
       </div>
-      {
-            items.map((e,index)=>{
-                return(
-                    <MovieCard key={index} title={e.title} image={items.image} category={items.category}/>
-                ) 
-                })
-                
-      }
-      {/* <MovieCard key={items[0].id} items={items[0]}/> */}
-      {movies?.length > 0 ? (
+
+      {movies?.length > 0 
+      ? 
+      (
         <div className="container">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} items={items}/>
+          {movies.map((movie,index) => (
+            <MovieCard
+              key={index}
+              movie={movie}
+            />
           ))}
         </div>
       ) : (
@@ -64,4 +52,3 @@ const App = () => {
   );
 };
 export default App;
- 
